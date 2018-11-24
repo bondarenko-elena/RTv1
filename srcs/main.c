72acf54bbb16161ee5fd3d8941fb3d3391790513
Olena BONDARENKO <olbondar@student.unit.ua>
2018-11-24 15:14:29 +0200

    commit

--- srcs/main.c
+++ srcs/main.c
@@ -15,14 +15,18 @@ int		expose_hook(t_env *env)
 
 int		main(int argc, char **argv)
 {
-	t_env env;
+	if (argc == 2)
+	{
+		t_env env;
 
-	pre_init_env(&env);
-	parser(&env, argc, argv);
-	post_init_env(&env);
-	mlx_hook(env.win, 2, 1L << 0, &key_press, &env);
-	mlx_expose_hook(env.win, expose_hook, &env);
-	mlx_hook(env.win, 17, 1L << 0, red_cross, &env);
-	mlx_loop(env.mlx);
-	return (1);
+		pre_init_env(&env);
+		parser(&env, argc, argv);
+		post_init_env(&env);
+		mlx_hook(env.win, 2, 1L << 0, &key_press, &env);
+		mlx_expose_hook(env.win, expose_hook, &env);
+		mlx_hook(env.win, 17, 1L << 0, red_cross, &env);
+		mlx_loop(env.mlx);
+	}
+	else ft_putendl("usage: ./rtv1 scenes/scene");
+	return (0);
 }
