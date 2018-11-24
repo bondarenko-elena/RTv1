33c95aebfc4e5377d2394b79afbd8c76b506cad2
Olena BONDARENKO <olbondar@student.unit.ua>
2018-11-24 15:55:50 +0200

    commit

--- srcs/init_environment.c
+++ srcs/init_environment.c
@@ -1,25 +1,25 @@
 #include "../includes/rtv1.h"
 
-void	pre_init_env(t_env *e)
+void	pre_init_env(t_env *env)
 {
 	t_vec3	normedemerde;
 
-	e->mlx = mlx_init();
+	env->mlx = mlx_init();
 	normedemerde = (t_vec3){0, 1, -5};
-	e->cam_pos = normedemerde;
+	env->cam_pos = normedemerde;
 	normedemerde = (t_vec3){0, 1, 0};
-	e->cam_dir = normedemerde;
-	e->obj = NULL;
-	e->objs = NULL;
-	e->ln = 0;
-	e->screen_name = "RTv1@42";
-	e->screen_width = 1000;
-	e->screen_height = 1000;
+	env->cam_dir = normedemerde;
+	env->obj = NULL;
+	env->objs = NULL;
+	env->ln = 0;
+	env->screen_name = "RTv1@4olbondar";
+	env->screen_height = HEIGHT;
+	env->screen_width = WIDTH;
 }
 
-void	post_init_env(t_env *e)
+void	post_init_env(t_env *env)
 {
-	e->win = mlx_new_window(e->mlx, e->screen_width, \
-			e->screen_height, e->screen_name);
-	e->ln = 1.0 / e->ln;
+	env->win = mlx_new_window(env->mlx, env->screen_width, \
+			env->screen_height, env->screen_name);
+	env->ln = 1.0 / env->ln;
 }
--- srcs/main.c
+++ srcs/main.c
@@ -23,8 +23,8 @@ int		main(int argc, char **argv)
 		parser(&env, argc, argv);
 		post_init_env(&env);
 		mlx_hook(env.win, 2, 1L << 0, &key_press, &env);
-		mlx_expose_hook(env.win, expose_hook, &env);
 		mlx_hook(env.win, 17, 1L << 0, red_cross, &env);
+		mlx_hook(env.win, 12, 1L << 0, expose_hook, &env);
 		mlx_loop(env.mlx);
 	}
 	else ft_putendl("usage: ./rtv1 scenes/scene");
